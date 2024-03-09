2f1bd7d1424fbbbde0df22e993cc0ce9097567fb
manavpradhan <manavpradhan36@gmail.com>
2024-03-09 22:27:12 +0530

    starting frontend

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-xl shadow-md bg-trannsparent bg-clip-padding backdrop-flter backdrop-blur-md">
+        <h1 className="text-3xl font-semibold text-center">LOGIN</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
