c37445ffb199976fb6c4112d4f298d744afc761b
manavpradhan <manavpradhan36@gmail.com>
2024-03-10 22:34:29 +0530

    login/signup page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
 
 function App() {
   return (
     <div className="p-5 h-screen flex justify-center items-center">
-      <Login />
+      <SignUp />
     </div>
   );
 }
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,14 +1,28 @@
 import React from "react";
+import GenderCheckBox from "./GenderCheckBox";
 
-const Login = () => {
+const SignUp = () => {
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-xl shadow-md bg-trannsparent bg-clip-padding backdrop-flter backdrop-blur-md">
         <h1 className="text-3xl font-semibold text-center caret-transparent pb-5">
-          LOGIN
+          SIGNUP
           <span className="text-orange-300 pl-3 caret-transparent">MyChat</span>
         </h1>
         <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Name</span>
+            </label>
+
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Your Fullname"
+              className="w-full input input-bordered h-12"
+            />
+          </div>
           <div>
             <label className="label p-2">
               <span className="text-base label-text">Username</span>
@@ -18,7 +32,7 @@ const Login = () => {
               type="text"
               name="username"
               id="username"
-              placeholder="Your username..."
+              placeholder="Your Username"
               className="w-full input input-bordered h-12"
             />
           </div>
@@ -31,16 +45,29 @@ const Login = () => {
               type="password"
               name="password"
               id="password"
-              placeholder="Your password..."
+              placeholder="Your Password"
               className="w-full input input-bordered h-12"
             />
           </div>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text">Confirm Password</span>
+            </label>
+
+            <input
+              type="password"
+              name="confirmpassword"
+              placeholder="Confirm Your Password"
+              className="w-full input input-bordered h-12"
+            />
+          </div>
+          <GenderCheckBox />
           <a className="text-sm hover:underline hover:text-orange-400 mt-5 inline-block cursor-pointer ml-40 caret-transparent">
-            Don't have an account?
+            Already have an account?
           </a>
           <div>
             <button className="btn btn-block btn-md mt-4 bg-orange-400 text-black hover:text-white">
-              LOGIN
+              SIGNUP
             </button>
           </div>
         </form>
@@ -49,4 +76,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
